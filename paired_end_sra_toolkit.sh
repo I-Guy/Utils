d9eb5685146cde254e5dcf701c5b0b5aadd26699
Ilango Guy <92375402+I-Guy@users.noreply.github.com>
2022-12-08 17:14:44 +0100

    Add files via upload
    Script to prefetch and download reads from sra toolkit

---
+++ paired_end_sra_toolkit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Author : Ilango Guy" &&
+echo "License :Research Center for Respiratory Diseases Tours - France" &&
+
+echo $1
+
+prefetch $1 &&
+
+fasterq-dump $1 -I --split-files --gzip &&
+
+echo "Process complete" &&
+
+echo "Don't forget to bring me a coffe"
+
+
+
+
+
+
